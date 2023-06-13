4b580463e740d902e513d22feb4c939764670909
zachjgraves <127673423+zachjgraves@users.noreply.github.com>
2023-06-13 14:57:54 -0700

    Update code-helper.py

--- code-helper.py
+++ code-helper.py
@@ -9,11 +9,13 @@ MODEL="text-davinci-003"
 
 st.title("GPT-3PO")
 st.subheader("Your friendly CODE helper")
+
+instructions=st.text_area("Add any additional instruction")
 ### Code Input Section 
 input_code=st.text_area("Input Your Code")
 ### Instructions Section
 language=st.selectbox("Pick a language", ["Python", "R", "JavaScript"])
-instructions=st.text_area("Add any additional instruction")
+
 
 if st.button("Submit"):
     prompt="Fix this code or translate if the language chosen does not match the code provided. \
@@ -22,8 +24,8 @@ if st.button("Submit"):
     response=openai.Completion.create(
         model=MODEL,
         prompt=prompt,
-        max_tokens=500,
-        temperature=0
+        max_tokens=1000,
+        temperature=0.2
     )
     st.caption(response.choices[0].text)
 
